5fe5b3aced91a08f6469cedf468385fcd1b6afec
piel berekhah <victorpaul.tg01@gmail.com>
2021-02-23 23:49:51 +0300

    add script.js

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-window.addEventListener("DOMContentLoaded", function () {
-    console.log("contentloadedSuccess")
-    let dateForm = document.querySelector("#date-form")
-    console.log("formAccepted")
 
-    dateForm.addEventListener("submit", function (submitClick) {
-        submitClick.preventDefault()
-        console.log("submitted");
+function getdata(){
+// window.addEventListener("DOMContentLoaded", function() {//
+//     console.log("contentloadedSuccess");
+//     let dateForm = document.querySelector("#date-form");
+//     console.log("formAccepted");
 
-        let gender = document.querySelector("#gender").value;
-        let date = new Date;
-        let birthday = date.getDay();
-        console.log(birthday);
+//     dateForm.addEventListener("submit", function (submitClick) {
+//         submitClick.preventDefault();
+//         console.log("submitted");
 
-        let male = ["Kwasi", "Kwadwo", "Kwabena", "Kaku", "Yaw", "Kofi", "Kwame"];
-        let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-        let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-        if (gender = "1") {
-            alert(male[birthday]);
-        }
-        else if (gender = "2") {
-            alert(female[birthday]);
-        } else {
-            alert("invalid input");
-        }
+//         let gender = document.querySelector("#gender").value;
+//         let userDate = document.querySelector("#date").value;
+//         let date = new Date;
+//         let birthday = date.getDay();
+//         userDate = birthday
+//         console.log(birthday);
 
-    })
-})  
+//         let CC = century
+//         let YY = year
+//         let MM = month
+//         let DD = day
+//         mod - is the modulus function ( % )
+//         let result =Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+//         let dayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//         let male = ["Kwasi", "Kwadwo", "Kwabena", "Kaku", "Yaw", "Kofi", "Kwame"];
+//         let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+//         if (gender == "true"); {
+//             //alert(male[userDate]);
+//         };
+//         else if (gender == "true"); {
+//             alert(female[userDate]);
+//         }else {
+//             alert("invalid input");
+//         }
+
+//         })
+//     })
